2226b9b55773a64c243da50ac04cb8d0e180ee4c
javaboy132 <javaboy132@gmail.com>
2017-06-20 23:55:40 +0000

    added commandline arguments to pi.py

--- pi.py
+++ pi.py
@@ -1,5 +1,31 @@
-pi=2
-for i in range(1000000):
-    pi=pi*((4*(i+1)**2)/(4*(i+1)**2-1))
+###################
+# (c)2017 Alex Coad
+# GPLv3
+#
+# This is a little command line tool that computes pi using the wallis equation
 
-print(pi)
+import sys,getopt
+
+def main(argv):
+    numiterations=3
+    helptext='pi.py -n <iterations>'
+    try:
+        opts, args=getopt.getopt(argv,"hn:",["numiterations="])
+    except getopt.GetoptError:
+        print(helptext)
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt=='-h':
+            print(helptext)
+            sys.exit()
+        elif opt in ("-n","--numiterations"):
+            numiterations=arg
+
+    pi=2
+    for i in range(arg):
+        pi=pi*((4*(i+1)**2)/(4*(i+1)**2-1))
+
+    print(pi)
+
+if __name__=="__main__":
+    main(sys.argv[1:])
