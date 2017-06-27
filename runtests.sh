76d15fcc12fba61f89ac232f18ef0dd5dfbbf8fc
javaboy132 <javaboy132@gmail.com>
2017-06-27 15:03:45 +0000

    added a script to pull TODO: comments from a source code file and store them in one convenient file for reference.

--- runtests.sh
+++ runtests.sh
@@ -7,4 +7,6 @@ echo testing texturegen.py
 python texturegen.py -o out.png --noshow -n 10 -b 1 -m rand3
 python texturegen.py -o out2.png --noshow -n 100 -b 2 -m rand1
 
+echo testing todoreader.py
+python todoreader.py -f test.c
 echo tests complete
---
+++ test.c
@@ -0,0 +1,6 @@
+#include <stdlib>
+//TODO: check the includes to make sure they're correct
+int main(){
+	printf("hello World\n");
+	return 0;
+}
